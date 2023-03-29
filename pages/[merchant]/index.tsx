ef9bff52d92f0eb64402bb1ea45de1b4a1922aec
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2023-03-30 02:24:59 +0700

    build: add helper link

--- pages/[merchant]/index.tsx
+++ pages/[merchant]/index.tsx
@@ -1,9 +1,13 @@
 import { NextPage } from 'next'
+import Link from 'next/dist/client/link'
 import React from 'react'
 
 const Page: NextPage = () => (
   <>
     <div>Hollo World!</div>
+    <Link href={{ pathname: '/[merchant]/serving-lists', query: { merchant: 'ruangtidur' } }}>
+      Go To List
+    </Link>
   </>
 )
 
