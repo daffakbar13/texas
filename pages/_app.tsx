67573c561c61c8a7bd733d48aa4a8fa4e0ec6003
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2023-03-29 12:04:55 +0700

    Initial commit from Create Next App

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import '@texas/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
