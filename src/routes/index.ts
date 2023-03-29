7b3a011870569aada4ff093ab88216627d593d26
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2023-03-29 15:56:34 +0700

    fix: error deploy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,1 @@
-import { authRoutes } from './auth.routes'
-import { banquetsRoutes } from './banquets.routes'
-import { employeesRoutes } from './employees.routes'
-import { othersRoutes } from './others.routes'
-import { overviewsRoutes } from './overviews.routes'
-import { reportsRoutes } from './reports.routes'
-
-export const routes = {
-  ...authRoutes,
-  ...overviewsRoutes,
-  ...banquetsRoutes,
-  ...employeesRoutes,
-  ...reportsRoutes,
-  ...othersRoutes,
-}
-
-export type RoutesType = keyof typeof routes
+export {}
