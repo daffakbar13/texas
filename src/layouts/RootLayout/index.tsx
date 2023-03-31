c8b4658fd695532c6f9debbc9fa9339f84b41a26
m_drakbar <daffaraihan03@gmail.com>
2023-03-31 14:58:10 +0700

    refactor: test push

--- src/layouts/RootLayout/index.tsx
+++ src/layouts/RootLayout/index.tsx
@@ -1,34 +1,17 @@
+import { Box } from '@mui/material'
 import React from 'react'
-
-const defaultTableProps: JSX.IntrinsicElements['table'] = {
-  role: 'presentation',
-  cellPadding: 0,
-  cellSpacing: 0,
-  width: '100%',
-}
 
 export default function RootLayout(props: React.PropsWithChildren) {
   const { children } = props
   return (
-    <table {...defaultTableProps}>
-      <tbody>
-        <tr>
-          <td align="center">
-            <table
-              {...defaultTableProps}
-              style={{ maxWidth: '600px', boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)' }}
-            >
-              <tbody>
-                <tr>
-                  <td valign="top" style={{ height: '100vh', overflow: 'scroll' }}>
-                    {children}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <Box
+      width="100%"
+      maxWidth={480}
+      height="100vh"
+      boxShadow="0px 0px 4px rgba(0, 0, 0, 0.25)"
+      margin="auto"
+    >
+      {children}
+    </Box>
   )
 }
--- src/views/serving-list/index.tsx
+++ src/views/serving-list/index.tsx
@@ -1,9 +1,10 @@
 import { Search } from '@mui/icons-material'
-import { Box, Collapse, IconButton, InputAdornment, OutlinedInput } from '@mui/material'
+import { Box, Button, Collapse, IconButton, InputAdornment, OutlinedInput } from '@mui/material'
 import React from 'react'
 import Image from 'next/image'
 import { ILDummyPromoBanner } from '@texas/assets'
 import { useMainStore } from '@texas/utils/stores'
+import ListRoundedIcon from '@mui/icons-material/ListRounded'
 
 export default function ServingList() {
   const { isOnSearch, setOnSearch } = useMainStore()
@@ -24,16 +25,42 @@ export default function ServingList() {
         onClick={() => setOnSearch(true)}
       />
       <Collapse in={!isOnSearch}>
-        <Box width="100%" height={96} position="relative">
-          <Image
-            src={ILDummyPromoBanner.src}
-            alt="Promo Banner"
-            fill
-            objectFit="cover"
-            style={{ borderRadius: 16 }}
-          />
+        <Box display="flex" flexDirection="column" gap={2} width="100%">
+          <Box width="100%" height={96} position="relative">
+            <Image
+              src={ILDummyPromoBanner.src}
+              alt="Promo Banner"
+              fill
+              objectFit="cover"
+              style={{ borderRadius: 16 }}
+            />
+          </Box>
+          <Box display="flex" gap={1} maxWidth={600 - 32} overflow="scroll">
+            <Button size="small" variant="outlined" sx={{ padding: 0.5, minWidth: 0 }}>
+              <ListRoundedIcon />
+            </Button>
+            <Box display="flex" gap={1} width="100%" overflow="scroll">
+              {[
+                'Foods',
+                'Drinks',
+                'Burger',
+                'Pizza',
+                'Tea',
+                'Coffee',
+                'Foods',
+                'Drinks',
+                'Burger',
+                'Pizza',
+                'Tea',
+                'Coffee',
+              ].map((e, i) => (
+                <Button key={i} size="small" variant={i === 0 ? 'contained' : 'outlined'}>
+                  {e}
+                </Button>
+              ))}
+            </Box>
+          </Box>
         </Box>
-        <div>Hello World!</div>
       </Collapse>
     </>
   )
